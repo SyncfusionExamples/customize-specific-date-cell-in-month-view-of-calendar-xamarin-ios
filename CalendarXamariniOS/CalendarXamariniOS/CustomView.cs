24ca491316a53ab938046cc68a61c7ea1322a6d4
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-08 12:56:41 +0530

    XAMARINIOS-3391 - Sample and screenshot added.

---
+++ CalendarXamariniOS/CalendarXamariniOS/CustomView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace CalendarXamariniOS
+{
+    public class CustomView : UIView
+    {
+        public CustomView() : base()
+        {
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+            this.Subviews[0].Frame = new CGRect(0, 20, this.Frame.Right, 50);
+        }
+    }
+}
---
+++ CalendarXamariniOS/CalendarXamariniOS/ViewController.cs
@@ -0,0 +1,65 @@
+﻿using CoreGraphics;
+using Foundation;
+using Syncfusion.SfCalendar.iOS;
+using System;
+using UIKit;
+
+namespace CalendarXamariniOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        SFCalendar calendar;
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            //// Initialize SfCalendar
+            calendar = new SFCalendar();
+            calendar.ViewMode = SFCalendarViewMode.SFCalendarViewModeMonth;
+
+            calendar.DrawMonthCell += CalendarDrawMonthCell;
+            this.View.AddSubview(calendar);
+        }
+
+        private void CalendarDrawMonthCell(object sender, DrawMonthCellEventArgs e)
+        {
+            NSCalendar date = NSCalendar.CurrentCalendar;
+            NSDate today = new NSDate();
+
+            NSDateComponents monthCellDateComponents = date.Components(
+                    NSCalendarUnit.Year | NSCalendarUnit.Month | NSCalendarUnit.Day, e.MonthCell.Date);
+            NSDateComponents todayDateComponents = date.Components(
+                  NSCalendarUnit.Year | NSCalendarUnit.Month | NSCalendarUnit.Day, today);
+
+            UIButton button = new UIButton();
+            button.SetTitle(monthCellDateComponents.Day.ToString(), UIControlState.Normal);
+            button.SetTitleColor(UIColor.Black, UIControlState.Normal);
+
+            if (e.MonthCell.IsCurrentMonth)
+            {
+                if (monthCellDateComponents.Day == todayDateComponents.Day && monthCellDateComponents.Month == todayDateComponents.Month && monthCellDateComponents.Year == todayDateComponents.Year)
+                {
+                    //// customized paricular date by setting differernt background color
+                    button.BackgroundColor = UIColor.Red;
+                }
+                else
+                {
+                    button.BackgroundColor = UIColor.LightGray;
+                }
+
+                e.MonthCell.View = button;
+            }
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
